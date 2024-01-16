71a45681ef42b7da8efc0f3a208214c7f40babbc
Mohammad <mohammadmns1379@gmail.com>
2024-01-16 11:37:25 +0330

    add-features

--- codes.py
+++ codes.py
@@ -1,9 +1,36 @@
 import requests
-
-from config import url
+import json
+from config import url, rules
 
 def get_rate():
     response = requests.get(url)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return None
+
+
+def archive(filename, rates):
+    with open(f'archive/{filename}.json', 'w') as f:
+        f.write(json.dumps(rates))
+
+def send_mail(timestamp, rates):
+    subject = f'{timestamp}'
+    if rules['currencies'] is not None:
+        tmp = dict()
+        for currency in rules['currencies']:
+            tmp[currency] = rates[currency]
+
+        rates = tmp
+    text = json.dumps(rates)
+
 
 if __name__ == '__main__':
-    get_rate()
+    res = get_rate()
+
+    if rules['archive']:
+        archive(res['timestamp'], res['rates'])
+
+    if rules['send_mail']:
+        send_mail(res['timestamp'], res['rates'])
+
+
--- config.py
+++ config.py
@@ -1,3 +1,11 @@
 BASE_PATH = "http://data.fixer.io/api/latest?access_key="
 API_KEY = "4d980eb40e4bc23f0c63292c819b6ec0"
 url = BASE_PATH + API_KEY
+
+
+rules = {
+    'archive' : True, 
+    'send_mail' : True, 
+#   'currencies' : None,
+    'currencies' : ['BTC', 'USD']
+}
